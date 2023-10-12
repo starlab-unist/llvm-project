51013e8903b8970a40e221fd7a6c825467d4f819
Sehoon <sehoon@unist.ac.kr>
2023-10-12 18:05:52 +0900

    renewal

--- clang-tools-extra/pathfinder-gen/parse.h
+++ clang-tools-extra/pathfinder-gen/parse.h
@@ -6,6 +6,8 @@
 extern bool option_class_done;
 
 std::unique_ptr<Param> parseTorchParam(clang::QualType t, std::string name, ASTContext &Ctx);
+
+// Base case
 Optional<std::unique_ptr<Param>> parseBuiltin(clang::QualType t, std::string name, ASTContext &Ctx);
 Optional<std::unique_ptr<Param>> parseDtype(clang::QualType t, std::string name, ASTContext &Ctx);
 Optional<std::unique_ptr<Param>> parseEnum(clang::QualType t, ASTContext &Ctx);
@@ -14,6 +16,8 @@ Optional<std::unique_ptr<Param>> parseTensor(clang::QualType t, std::string name
 Optional<std::unique_ptr<Param>> parseIntArrayRef(clang::QualType t, std::string name, ASTContext &Ctx);
 Optional<std::unique_ptr<Param>> parseExpandingArray(clang::QualType t, std::string name, ASTContext &Ctx);
 Optional<std::unique_ptr<Param>> parseExpandingArrayWithOptionalElem(clang::QualType t, std::string name, ASTContext &Ctx);
+
+// Recursive case
 Optional<std::unique_ptr<Param>> parseOptional(clang::QualType t, std::string name, ASTContext &Ctx);
 Optional<std::unique_ptr<Param>> parseVariant(clang::QualType t, std::string name, ASTContext &Ctx);
 Optional<std::unique_ptr<Param>> parseMAP(clang::QualType t, std::string name, ASTContext &Ctx);
